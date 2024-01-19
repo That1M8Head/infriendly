4fa232c9afb8fe809620ad7758d8ffb74cc8df00
Arsalan Kazmi <sonicspeed848@gmail.com>
2024-01-19 15:06:17 +0000

    Added functionality for loading STARTUP.txt

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 /* app.js - Infriendly's functionality. */
+
+function loadFromFile(fileName, callback) {
+    var reader = new FileReader();
+
+    reader.onload = (e) => {
+        const fileContents = e.target.result;
+        callback(fileContents);
+    }
+
+    reader.readAsText(fileName);
+}
+
+function setTextAreaContent(content) {
+    const textArea = document.querySelector("main");
+    
+    const formattedContent = content.replace(/\n/g, '<br>');
+    
+    textArea.innerHTML = formattedContent;
+}
+
+window.addEventListener("load", () => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const fileContents = xhr.responseText;
+            setTextAreaContent(fileContents);
+        }
+    };
+
+    xhr.open("GET", "STARTUP.txt", true);
+    xhr.send();
+});
